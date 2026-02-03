9ffd277d09609b3231e5eacf36bd99718a89fe09
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2026-02-03 13:19:30 -0600

    Implementación del proyecto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Administrator admin = new Administrator();
 
+        System.out.print("Usuario: ");
+        String user = input.nextLine();
+
+        System.out.print("Contraseña: ");
+        String p = input.nextLine();
+
+        if (admin.authenticateUser(user, p)) {
+
+            MainSystem system = new MainSystem();
+            system.menu();
+
+        } else {
+            System.out.println("Acceso denegado.");
+        }
     }
 }

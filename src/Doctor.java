c71dd05b3a52e14d63e71ce49756fe08da8e7770
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2026-02-03 11:40:18 -0600

    Creación de clases principales

---
+++ src/Doctor.java
@@ -0,0 +1,11 @@
+public class Doctor {
+    private String name;
+    private String speciality;
+    private String ID;
+
+    public Doctor(String name, String speciality, String ID) {
+        this.name = name;
+        this.speciality = speciality;
+        this.ID = ID;
+    }
+}
---
+++ src/Patient.java
@@ -0,0 +1,9 @@
+public class Patient {
+    private String name;
+    private String medicalRecordNumber;
+
+    public Patient(String name, String medicalRecordNumber) {
+        this.name = name;
+        this.medicalRecordNumber = medicalRecordNumber;
+    }
+}
